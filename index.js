df0a9fdc262c6a09bd5d49d18e527184bb189917
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-03-25 02:13:42 +0100

    Done, with bonuses.

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.item = this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.indexOf(pos) === -1) {
+      throw new Error("OutOfBounds");
+    }
+    return (this.pos = this.items.indexOf(pos));
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
-
-  avg() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0
+    }
+    let sum = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      sum += this.items[i];
+    }
+    return sum;
+  }
+  avg() {
+  if (this.items.length === 0) {
+  throw new Error('EmptySortedList');
+  }
+  return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
